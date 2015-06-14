a94cd9d9c04996e26cafbe9b822f119b8971bf8c
Paul Haggart <ph@storm.ca>
2015-06-13 20:12:39 -0400

    seat requires https, update the documentation to match

--- Auth_SeAT.php
+++ Auth_SeAT.php
@@ -20,9 +20,9 @@
 // require_once('extensions/Auth_SeAT.php');
 // $wgAuth_Config['api_user'] = 'mediawiki_auth';
 // $wgAuth_Config['api_pass'] = 'mypassword';
-// $wgAuth_Config['api_url'] = 'http://myseatapiurl/api/v1/authenticate';
+// $wgAuth_Config['api_url'] = 'https://myseatapiurl/api/v1/authenticate';
 // $wgAuth = new Auth_SeAT($wgAuth_Config);
- 
+
 if (!defined('MEDIAWIKI')) exit;
 
 require_once('includes/AuthPlugin.php');
